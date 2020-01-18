a9340798003876019701f316159d6f18782adfb0
Phoenix R <phoenixrabang@gmail.com>
2020-01-18 22:54:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color ="violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(colors_in_the_rainbow, firstcity)
+  firstcity= "Staten Island"
+  colors_in_the_rainbow.unshift(firstcity)
+end
+
+def using_pop(colors_in_the_rainbow,lastcity)
+  lastcity = colors_in_the_rainbow.pop
+  p colors_in_the_rainbow
+end
