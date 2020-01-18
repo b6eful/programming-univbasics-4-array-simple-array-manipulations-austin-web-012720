d98033697bc85cb3354675e14ad1d989386a4b58
Phoenix R <phoenixrabang@gmail.com>
2020-01-18 22:56:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,7 +9,7 @@ def using_unshift(colors_in_the_rainbow, firstcity)
   colors_in_the_rainbow.unshift(firstcity)
 end
 
-def using_pop(continents,newcont)
+def using_pop(continents,continents)
   newcont = colors_in_the_rainbow.pop
   p colors_in_the_rainbow
 end
